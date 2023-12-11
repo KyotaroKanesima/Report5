4c004758e556cbe05a245dee355b53bd5f3f587a
Kyotaro Kaneshima <e235727@ie.u-ryukyu.ac.jp>
2023-12-12 02:33:15 +0900

    third commit

--- src/jp/ac/uryukyu/ie/e235727/Exception1.java
+++ src/jp/ac/uryukyu/ie/e235727/Exception1.java
@@ -1,9 +1,14 @@
 package  jp.ac.uryukyu.ie.e235727;
+
+
 public class Exception1 {
+    
     public static void main(String[] args) {
         String str;
-        str = null;
-        System.out.println(str.length());
+        int value;
+        str = "壱百満";
+        Integer.parseInt(value);
+        System.out.println(Integer.parseInt(value));
     }
     
 }
